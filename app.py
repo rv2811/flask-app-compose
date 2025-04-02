7b35ce520e84625410249f35e0e37e0f1a05939c
root <root@test11.h2wohyjreuvudox4qb1nv3hprg.gx.internal.cloudapp.net>
2025-04-02 05:36:53 +0000

    added jenkins file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Import necessary modules from Flask framework v1o
+# Import necessary modules from Flask framework v1qq
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
